2d7f9dbff6c424f348df7fc3b6557d1c7987af27
Yuriy <yurii.choboda@gmail.com>
2023-12-05 12:58:10 +0200

    navigation

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import Home from "./src/screens/Home";
+import Expenses from "./src/screens/Expenses";
+
+
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <BottomTab.Navigator screenOptions={{
+      headerStyle: { backgroundColor: '#fff'  },
+      headerTintColor: 'black',
+      tabBarStyle: { backgroundColor: '#fff' },
+    }}>
+      <BottomTab.Screen name="Home" component={Home}/>
+      <BottomTab.Screen name="Expenses" component={Expenses}/>
+    </BottomTab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="auto"/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} options={{headerShown: false}} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
